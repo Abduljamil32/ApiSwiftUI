76a7e749c369a1d91375ef3ad1409fd26c252d5d
iMtRo <maxmudovilyos75@gmail.com>
2022-05-06 15:30:46 +0500

    Initial Commit

---
+++ ApiSwiftUI/ApiSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiSwiftUIApp.swift
+//  ApiSwiftUI
+//
+//  Created by user on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ApiSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApiSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ApiSwiftUI
+//
+//  Created by user on 5/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
